830066f182dc3670bc125e12d827f1b1ba783799
unknown <sashazhur515@gmail.com>
2024-10-25 17:09:49 +0300

    add index page

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Журавлев Александр Сергеевич";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
